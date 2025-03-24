fa9567263c10fae9420562653c272a9a60e8ec6d
RafaCruzX <rafinhacruznasci@gmail.com>
2025-03-24 15:27:30 -0300

    Add files via upload

---
+++ Usuario.js
@@ -0,0 +1,13 @@
+const usuarios =[
+    {id: 1,nome:"Rafaela Cruz", id_grupo: 1},
+    {id: 2,nome:"Filipe Roberto", id_grupo: 2},
+    {id: 3,nome:"Enzo Silva", id_grupo: 3},
+    {id: 4,nome:"Neymar Junior", id_grupo: 4},
+    {id: 5,nome:"Pelé", id_grupo: 5},
+    {id: 6,nome:"Chave Racha Arajá", id_grupo: 6},
+    {id: 7,nome:"Nicolas Coutinho", id_grupo: 7},
+    {id: 8,nome:"Abdnego Cruz", id_grupo: 8},
+    {id: 9,nome:"Larissa Santos", id_grupo: 9},
+    {id: 10,nome:"Alan Jeon", id_grupo: 10},
+
+]
